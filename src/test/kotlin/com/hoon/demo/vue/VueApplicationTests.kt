94639baff6e9505651c4dd11b9390394c3a83d36
sherlock.hoons <sherlock.hoons@kakaopaycorp.com>
2022-03-24 16:34:40 +0900

    Init project

---
+++ src/test/kotlin/com/hoon/demo/vue/VueApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hoon.demo.vue
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VueApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
